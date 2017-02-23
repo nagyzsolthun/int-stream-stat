ff333b2043101fdc1b64de63c3bc18513bc7debe
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2017-02-24 00:12:08 +0100

    read from stdin

--- src/main/java/zsolt/app/App.java
+++ src/main/java/zsolt/app/App.java
@@ -1,13 +1,22 @@
 package zsolt.app;
 
+import java.util.Scanner;
+
+import zsolt.app.impl.MappedIntegerStreamStat;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+    	IntegerStreamStat stat = new MappedIntegerStreamStat(1000);
+    	Scanner scanner = new Scanner(System.in);
+    	while(scanner.hasNextInt()) {
+    		stat.add(scanner.nextInt());
+    	}
+    	scanner.close();
+    	
+    	System.out.println("median is: " + stat.calcMedian());
     }
 }
